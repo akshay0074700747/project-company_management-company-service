41ab7dd6e45a7cc4deb958f456ef504e02ab3525
akshay <ask029849@gmail.com>
2024-03-22 11:39:48 +0530

    slight changes

--- internal/services/consumer.go
+++ internal/services/consumer.go
@@ -103,6 +103,7 @@ func (company *CompanyServiceServer) StartConsuming() {
 	config := sarama.NewConfig()
 	config.Consumer.Return.Errors = true
 	config.Metadata.AllowAutoTopicCreation = true
+	config.Consumer.Offsets.AutoCommit.Enable = true
 	consumer, err := sarama.NewConsumer([]string{"host.docker.internal:9092"}, config)
 	if err != nil {
 		log.Fatalf("Error creating consumer: %v", err)
